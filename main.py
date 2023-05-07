60192fc473d9c4c6816d36b6ac56b3180b3db53c
Vincent Lee <vlee2@nevada.unr.edu>
2023-05-06 22:02:47 -0700

    increased population, nodes, battery life, and statistical analysis

--- main.py
+++ main.py
@@ -2,35 +2,46 @@ import networkx as nx
 import numpy as np
 import random
 
-matrix = np.array([[0, 1, 1, 0],
-         [1, 0, 1, 1],
-         [1, 1, 0, 1],
-         [0, 1, 1, 0]])
+# 6 node adj matrix
+matrix = np.array([[0, 1, 0, 0, 1, 1],
+                   [1, 0, 1, 0, 0, 1],
+                   [0, 1, 0, 1, 0, 1],
+                   [0, 0, 1, 0, 1, 1],
+                   [1, 0, 0, 1, 0, 1],
+                   [1, 1, 1, 1, 1, 0]])
 
-energy = 40
+# Hop capacity of each node
+energy = 50
 
-random.seed()
-G = nx.from_numpy_array(matrix, create_using=nx.DiGraph())
-nx.set_node_attributes(G, energy, 'energy')
+# Initiate graph network and node batteries
+sensor_network = nx.from_numpy_array(matrix, create_using=nx.DiGraph())
+nx.set_node_attributes(sensor_network, energy, 'energy')
 
 
-def simulate(graph):
+def simulate(graph, display):
+    random.seed()
+    G = graph.copy()
     transmissions = 0
-    network_cond = 1
-    while network_cond:
-        u, v = random.sample(range(0, 4), 2)
-        path = nx.dijkstra_path(graph, u, v)
+    while True:
+        u, v = random.sample(range(0, 6), 2)
+        path = nx.dijkstra_path(G, u, v)
         transmissions += 1
-        print(f'T{transmissions}: {path}')
+        if display:
+            print(f'T{transmissions}: {path}')
         for node in path[:-1]:
-            graph.nodes[node]['energy'] -= 1
-            for adj in graph.adj[node]:
-                graph.adj[node][adj]['weight'] += 1
-            if graph.nodes[node]['energy'] <= 0:
-                print(f"Node {node} is out of energy")
-                print(dict(graph.nodes))
-                print(f'Completed: {transmissions} transmissions')
-                network_cond = 0
+            G.nodes[node]['energy'] -= 1
+            for adj in G.adj[node]:
+                G.adj[node][adj]['weight'] += 1
+            if G.nodes[node]['energy'] <= 0:
+                if display:
+                    print(f"Node {node} is out of energy!")
+                    for n in range(len(G.nodes)):
+                        print(f"Node {n} | Battery Left: {G.nodes[n]['energy']} hops")
+                    print(f'Completed: {transmissions} transmissions')
+                return transmissions
 
 
-simulate(G)
+runs = []
+for _ in range(100):
+    runs.append(simulate(sensor_network, False))
+print(f'Average Transmission Longevity: {sum(runs)/len(runs)}')
