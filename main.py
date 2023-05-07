33271fdf6ac49b45eb2391031a80340ac1d03fa2
Lloyd Gonzales <lgonzalesna@nevada.unr.edu>
2023-05-07 14:00:56 -0700

    Modularize components, always use node 0 as destination per paper

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 """
-
+import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import random
+from typing import Union
+from itertools import combinations, groupby
 
 # Simulation function (Set Display flag for individual paths taken)
 def simulate(graph: nx.DiGraph, display: bool):
@@ -34,9 +36,18 @@ def simulate(graph: nx.DiGraph, display: bool):
     energy_used = 0
 
     while True:
+        # TODO: The paper currently mentions that all nodes go to a C2, which I've
+        # assumed to be node 0. This is currently hardcoded (and naturally, if
+        # we decide to go this route, random.sample should be random.randint).
+        #
+        # Still thinking on the easiest thing to write about - should the focus
+        # of the report be on testing a bunch of different networks with the
+        # same algorithm, or on testing the same network with different algorithms?
+
         # Sample a random source and destination
-        u, v = random.sample(range(len(G.nodes)), 2)
-        path = nx.dijkstra_path(G, u, v)
+        u, v = random.sample(range(len(G.nodes)), 2) 
+        path = nx.dijkstra_path(G, u, 0)
+
         transmissions += 1
         if display:
             print(f"T{transmissions}: {path}")
@@ -58,10 +69,34 @@ def simulate(graph: nx.DiGraph, display: bool):
                         print(f"Node {n} | Battery Left: {G.nodes[n]['energy']} hops")
                     print(f"Completed: {transmissions} transmissions")
 
-                return transmissions, energy_used / len(G.nodes)
+                return transmissions, energy_used / len(G.nodes), G
 
+def gnp_random_connected_graph(n, p) -> nx.Graph:
+    """
+    Generates a random undirected graph, similarly to an Erdős-Rényi 
+    graph, but enforcing that the resulting graph is conneted
+    """
+    # From https://stackoverflow.com/questions/61958360/how-to-create-random-graph-where-each-node-has-at-least-1-edge-using-networkx
+    edges = combinations(range(n), 2)
+    G = nx.Graph()
+    G.add_nodes_from(range(n))
+    if p <= 0:
+        return G
+    if p >= 1:
+        return nx.complete_graph(n, create_using=G)
+    for _, node_edges in groupby(edges, key=lambda x: x[0]):
+        node_edges = list(node_edges)
+        random_edge = random.choice(node_edges)
+        G.add_edge(*random_edge)
+        for e in node_edges:
+            if random.random() < p:
+                G.add_edge(*e)
+    return G
 
-if __name__ == "__main__":
+def baseline_graph() -> nx.DiGraph:
+    """
+    The "baseline" directed graph.
+    """
     # Adjacency matrix for six nodes. View the report for a visual representation
     # of the network this represents.
     matrix = np.array(
@@ -74,22 +109,93 @@ if __name__ == "__main__":
             [1, 1, 1, 1, 1, 0],
         ]
     )
+    sensor_network = nx.from_numpy_array(matrix, create_using=nx.DiGraph())
 
+def initialize_graph(
+    graph: Union[nx.Graph, nx.DiGraph],
+    initial_energy: int = 50
+) -> nx.DiGraph:
+    """
+    Return an initialized graph for simulation.
+
+    If the graph is undirected, the graph is converted to a digraph.
+    The original graph is not modified.
+
+    :param graph: The graph to initialize.
+    :param initial_energy: The inital energy
+    :returns: A digraph with the necessary attributes initialized.
+    """
+    # Create copy of incoming graph
+    G = graph.copy()
+
+    # If the graph is undirected, make it a directed graph with edges
+    # in both directions (i.e. the graph is functionally the same)
+    if isinstance(G, nx.Graph):
+        G = G.to_directed()
+
+    # Initialize node batteries and weight "hints"
+    nx.set_node_attributes(G, initial_energy, "energy")
+    nx.set_edge_attributes(G, 0, "weight")
+
+    return G
+
+def draw_graph(graph: nx.DiGraph) -> None:
+    """
+    Display the digraph using the report's conventions for graph-making.
+
+    Note that `graph` should have already been run through the simulator.
+
+    :param graph: The graph to visualize using matplotlib.
+    """
+    # Create "base" plot
+    plt.figure(figsize=(10,6))
+
+    # Calculate positions of nodes according to the spring layout,
+    # which has been the most visually "appealing" in our tests
+    positions = nx.spring_layout(graph)
+
+    # Draw the graph using the report's conventions. Note that the weight
+    # attribute of edges is added as well. Formally, the edges are in both directions
+    # (because nodes should always be able to transmit to each other), but
+    # the "advertised" edge weight is the only thing that matters, since it indicates
+    # how much or how little that edge should be used in trying to get to the C2.
+    node_labels = nx.get_node_attributes(graph, 'energy')
+    nx.draw(
+        graph,
+        pos=positions,
+        node_color='lightblue', 
+        with_labels=True, 
+        node_size=500,
+    )
+    labels = nx.get_edge_attributes(graph, 'weight')
+    nx.draw_networkx_edge_labels(
+        graph, 
+        pos=positions, 
+        edge_labels=labels,
+    )
+
+    plt.show()
+
+if __name__ == "__main__":
     # The "battery" of each node. This is equivalent to the number of transmissions
     # each node can perform before dying, at which point the simulation should stop.
     energy = 50
 
-    # Initiate graph network and node batteries
-    sensor_network = nx.from_numpy_array(matrix, create_using=nx.DiGraph())
-    nx.set_node_attributes(sensor_network, energy, "energy")
+    # Using random graphs for now. The idea is that a suitable graph for simulation
+    # can easily be swapped out for another valid graph/diagraph by just calling
+    # initialze_graph.
+    sensor_network = gnp_random_connected_graph(12, 0.01)
+    sensor_network = initialize_graph(sensor_network, initial_energy=energy)
 
-    # Execute 100 simulations.
+    # Execute 100 simulations on the same network, 'reinitialized" each time.
     avg_tr = []
     avg_eg = []
     for _ in range(100):
-        t, e = simulate(sensor_network, False)
+        t, e, final_graph = simulate(sensor_network, True)
         avg_tr.append(t)
         avg_eg.append(e)
+
+        draw_graph(graph)
 
     # Print results.
     print(
