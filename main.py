474bd4525b891268e6c27be47b1511ed64d6350e
lgactna <lgonzalesna@gmail.com>
2023-05-09 11:13:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 """
+NOTE: Running all simulations in __main__ may fail, since the generation of a powerlaw
+tree is non-deterministic and may not succeed within the default number of attempts.
+
+To fix this, remove the Powerlaw Tree tuple entry from `topologies` in main(), or 
+re-run the script until generation succeeds. This is a constraint of NetworkX.
 """
 import matplotlib.pyplot as plt
 import networkx as nx
